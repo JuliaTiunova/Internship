734c17083e68f07df9b048af8be3704b03fc7ff1
Yuliia Tiunova <yuliia.tiunova@valtech.com>
2021-09-20 10:57:06 +0300

    Add deals section: add font icomoon, fix icons, add background-img for icons

--- src/slider.js
+++ src/slider.js
@@ -1,29 +1,41 @@
-import * as $ from 'jquery'
-import slick from 'slick-carousel'
+import * as $ from "jquery";
+import slick from "slick-carousel";
 
-export default 
-$('.header__slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 1000,
-    arrows: false,
-    asNavFor: '.categories'
+export default $(".header__slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 1000,
+  arrows: false,
+  asNavFor: ".categories",
+});
+$(".categories").slick({
+  slidesToShow: 5,
+  asNavFor: ".slider",
+  focusOnSelect: true,
+  arrows: false,
+});
 
-})
-$('.categories').slick({
-    slidesToShow: 5,
-    asNavFor: '.slider',
-    focusOnSelect: true,
-    arrows: false
-})
-
-$('.arrival__slider').slick({
-    slidesToShow: 5,
-    slidesToScroll: 2,
-    autoplay: true,
-    speed: 1000,
-    arrows: true,
-})
-
-
+$(".arrival__slider").slick({
+  slidesToShow: 5,
+  slidesToScroll: 2,
+  autoplay: true,
+  speed: 1000,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1920,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
