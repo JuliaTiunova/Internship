3f6fd7ce774d5322654ec939c71523681177c2f3
Yuliia Tiunova <yuliia.tiunova@valtech.com>
2021-09-22 20:20:02 +0300

    setup eslint, fix slider import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import * as $ from "jquery";
 import "./babel";
 import "./styles/styles.scss";
 import "./slider";
-import slick from "slick-carousel";
+
 import cart from "./cart";
 import burger from "./burger";
 import countdown from "./countdown";
--- webpack.config.js
+++ webpack.config.js
@@ -1,148 +1,159 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const TerserWebpackPlugin = require('terser-webpack-plugin')
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
 
-
-const isDev = process.env.NODE_ENV === 'development'
-const isProd = !isDev
+const isDev = process.env.NODE_ENV === "development";
+const isProd = !isDev;
 
 const optimization = () => {
-    const config =  {
-        splitChunks: {
-            chunks: 'all'
-        }
-    }
+  const config = {
+    splitChunks: {
+      chunks: "all",
+    },
+  };
 
-    if (isProd) {
-        config.minimizer = [
-            new OptimizeCssAssetsPlugin(),
-            new TerserWebpackPlugin()
-        ]
-    }
-    return  config
-}
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsPlugin(),
+      new TerserWebpackPlugin(),
+    ];
+  }
+  return config;
+};
 
-const cssLoaders = extra => {
-    const loaders =  [MiniCssExtractPlugin.loader, 'css-loader']
+const cssLoaders = (extra) => {
+  const loaders = [MiniCssExtractPlugin.loader, "css-loader"];
 
-    if (extra) {
-        loaders.push(extra)
-    }
+  if (extra) {
+    loaders.push(extra);
+  }
 
-    return loaders
-}
+  return loaders;
+};
 
+const jsLoaders = () => {
+  const loader = [
+    {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+      },
+    },
+  ];
+
+  if (isDev) {
+    loader.push("eslint-loader");
+  }
+  return loader;
+};
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: 'development',
-    entry: {
-        main: ['@babel/polyfill', './index.js']
-    },
-    optimization: optimization(),
-    devServer: {
-        port: 4200,
-        static: './dist',
-    },
-    output: {
-        filename: '[name].[hash].js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-           
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                // type: 'asset/resource'
-                use: [
-                    {
-                      loader: 'image-webpack-loader',
-                      options: {
-                        mozjpeg: {
-                          progressive: true,
-                        },
-                        // optipng.enabled: false will disable optipng
-                        optipng: {
-                          enabled: false,
-                        },
-                        pngquant: {
-                          quality: [0.65, 0.90],
-                          speed: 4
-                        },
-                        gifsicle: {
-                          interlaced: false,
-                        },
-                        // the webp option will enable WEBP
-                        webp: {
-                          quality: 75
-                        }
-                      }
-                    },
-                  ],
-               
+  context: path.resolve(__dirname, "src"),
+  mode: "development",
+  entry: {
+    main: ["@babel/polyfill", "./index.js"],
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 4200,
+    static: "./dist",
+  },
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        // type: 'asset/resource'
+        use: [
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
             },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ['csv-loader'],
-            },
-            {
-                test: /\.xml$/i,
-                use: ['xml-loader'],
-            },
-            {
-                test: /\.css$/i,
-                use: cssLoaders()
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: cssLoaders('sass-loader')
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
-                }
-              }
-            
-        ]
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-         template: './index.html',
-         minify: {
-             collapseWhitespace: isProd
-         }
-        }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, 'src/beetroot.png'), 
-                    to: path.resolve(__dirname, 'dist')
-                },
-                {
-                    from: path.resolve(__dirname, 'src/img'),
-                    to: path.resolve(__dirname, 'dist/img')
-                }
-            ]
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[hash].css'
-        })
-
-    ]
-  
-}
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ["csv-loader"],
+      },
+      {
+        test: /\.xml$/i,
+        use: ["xml-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: cssLoaders("sass-loader"),
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+        // use: {
+        //   loader: "babel-loader",
+        //   options: {
+        //     presets: ["@babel/preset-env"],
+        //   },
+        //   plugins: ["babel-eslint"],
+        // },
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./index.html",
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src/beetroot.png"),
+          to: path.resolve(__dirname, "dist"),
+        },
+        {
+          from: path.resolve(__dirname, "src/img"),
+          to: path.resolve(__dirname, "dist/img"),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[hash].css",
+    }),
+  ],
+};
