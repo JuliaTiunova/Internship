dd4360f038d87c6bab03b157a35c07217a7778b5
Yuliia Tiunova <yuliia.tiunova@valtech.com>
2021-09-29 19:20:30 +0300

    fix form validation, create separate scss file for forms

--- src/forms.js
+++ src/forms.js
@@ -1,4 +1,4 @@
-import "./styles/styles.scss";
+import "./styles/forms.scss";
 import datepicker from "js-datepicker";
 import * as $ from "jquery";
 import "jquery-mask-plugin";
@@ -100,6 +100,8 @@ const filePhoto = document.querySelector(".checkout-file");
 const form = document.querySelector(".checkout__form");
 const errorMessage = document.querySelector(".error-message");
 
+$(".checkout__form").attr("autocomplete", "off");
+
 username.addEventListener("input", () => {
   if (username.validity.valueMissing) {
     username.setCustomValidity(`Please enter a username`);
@@ -124,207 +126,288 @@ password.addEventListener("input", () => {
   }
 });
 
+function showErrorMessage(message, element) {
+  errorMessage.className = "error-message active";
+  errorMessage.textContent = message;
+  element.classList.add("error");
+  element.focus();
+}
+
+function removeError(element) {
+  errorMessage.className = "error-message";
+  element.classList.remove("error");
+}
+
+const transformFirstLetter = (el) => {
+  el.value = el.value[0].toLocaleUpperCase() + el.value.slice(1);
+};
+
 form.addEventListener("submit", (e) => {
-  if (!email.validity.valid || email.value === "") {
+  if (email.value === "") {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter an e-mail";
-  } else if (!firstName.validity.valid || firstName.value === "") {
+    showErrorMessage("Please enter an e-mail", email);
+  } else if (email.classList.contains("error")) {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter your name";
-  } else if (!lastName.validity.valid || lastName.value === "") {
+    showErrorMessage("Please enter a valid e-mail", email);
+  } else if (firstName.value === "") {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter your last name";
-  } else if (!postalCode.validity.valid || postalCode.value === "") {
+    showErrorMessage("Please enter your name", firstName);
+  } else if (firstName.classList.contains("error")) {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter a real postcode";
-  } else if (!phoneNumber.validity.valid || phoneNumber.value === "") {
+    showErrorMessage(`Please enter your real name`, firstName);
+  } else if (lastName.value === "") {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter a real phone number";
-  } else if (!cardNumber.validity.valid || cardNumber.value === "") {
+    showErrorMessage(
+      `Please enter your last name, must be at least ${lastName.minLength} characters`,
+      lastName
+    );
+  } else if (lastName.classList.contains("error")) {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter your card number";
-  } else if (!cardFullName.validity.valid || cardFullName.value === "") {
+    showErrorMessage(`Please check your last name`, lastName);
+  } else if (postalCode.value === "") {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter the full name";
-  } else if (!cardExpiration.validity.valid || cardExpiration.value === "") {
+    showErrorMessage("Please enter postcode", postalCode);
+  } else if (postalCode.classList.contains("error")) {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter the expiration date";
-  } else if (!cardCvv.validity.valid || cardCvv.value === "") {
+    showErrorMessage("Please enter a real postcode", postalCode);
+  } else if (phoneNumber.value === "") {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter a CVV number";
-  } else if (!tip.validity.valid) {
+    showErrorMessage(`Please enter a phone number`, phoneNumber);
+  } else if (phoneNumber.classList.contains("error")) {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please give us some money";
-  } else if (!url.validity.valid) {
+    showErrorMessage(`Please enter a valid phone number`, phoneNumber);
+  } else if (cardNumber.value === "") {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter an URL";
-  } else if (!filePhoto.validity.valid) {
+    showErrorMessage("Please enter your card number", cardNumber);
+  } else if (cardNumber.classList.contains("error")) {
     e.preventDefault();
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `file must be no bigger then 1Mb`;
+    showErrorMessage(`Please enter more digits`, cardNumber);
+  } else if (cardFullName.value === "") {
+    e.preventDefault();
+    showErrorMessage(`Please enter the full name`, cardFullName);
+  } else if (cardFullName.classList.contains("error")) {
+    e.preventDefault();
+    showErrorMessage(`Please enter the full name`, cardFullName);
+  } else if (cardExpiration.value === "") {
+    e.preventDefault();
+    showErrorMessage("Please enter the expiration date", cardExpiration);
+  } else if (cardExpiration.classList.contains("error")) {
+    e.preventDefault();
+    showErrorMessage(`Please enter a valid expiration date`, cardExpiration);
+  } else if (cardCvv.value === "") {
+    e.preventDefault();
+    showErrorMessage("Please enter a CVV number", cardCvv);
+  } else if (cardCvv.classList.contains("error")) {
+    e.preventDefault();
+    showErrorMessage("Please enter a CVV number", cardCvv);
+  } else if (tip.value === "") {
+    e.preventDefault();
+    showErrorMessage(`Please give us some money`, tip);
+  } else if (tip.classList.contains("error")) {
+    e.preventDefault();
+    showErrorMessage(`Why so cheap? Only $${tip.value}?`, tip);
+  } else if (url.classList.contains("error")) {
+    e.preventDefault();
+    showErrorMessage("Please enter a valid URL", url);
+  } else if (filePhoto.classList.contains("error")) {
+    e.preventDefault();
+    showErrorMessage(`file must be a photo`, filePhoto);
   }
 });
 
 email.addEventListener("input", function() {
-  if (email.validity.typeMismatch) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter a valid email adress";
-  } else if (email.value === "") {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = "Please enter an email";
+  let emailValue = email.value.split("@");
+  console.log(emailValue);
+  let beforeDot;
+  if (emailValue[1]) {
+    beforeDot = emailValue[1].split(".");
+  }
+  console.log(beforeDot);
+
+  if (email.value === "") {
+    showErrorMessage("Please enter an e-mail", email);
+  } else if (!emailValue[1] || !beforeDot[1] || beforeDot[1].length < 2) {
+    showErrorMessage("Please enter a valid e-mail", email);
   } else {
-    email.setCustomValidity("");
-    errorMessage.className = "error-message";
+    removeError(email);
   }
 });
 
 firstName.addEventListener("input", function() {
-  if (firstName.validity.tooShort) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter your real name, must be at least ${firstName.minLength} characters`;
-  } else if (firstName.validity.tooLong) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Your name cannot have ${firstName.value} characters. Max is ${firstName.maxlength}`;
-  } else {
-    firstName.setCustomValidity("");
+  let regExp = /^[a-zA-Z]+$/;
 
-    errorMessage.className = "error-message";
+  if (firstName.value === "") {
+    showErrorMessage("Please enter your name", firstName);
+  } else if (firstName.validity.tooShort) {
+    transformFirstLetter(firstName);
+    showErrorMessage(
+      `Please enter your real name, must be at least ${firstName.minLength} characters`,
+      firstName
+    );
+  } else if (!regExp.test(firstName.value)) {
+    transformFirstLetter(firstName);
+    showErrorMessage(`Be sure to enter only letters`, firstName);
+  } else {
+    transformFirstLetter(firstName);
+    removeError(firstName);
   }
 });
 
 lastName.addEventListener("input", function() {
-  if (lastName.validity.tooShort) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter your last name, must be at least ${lastName.minLength}`;
-  } else if (lastName.validity.tooLong) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Your last name cannot be ${lastName.value}. Max is ${lastName.maxlength}`;
-  } else {
-    lastName.setCustomValidity("");
+  let regExp = /^[a-zA-Z]+$/;
 
-    errorMessage.className = "error-message";
+  if (lastName.value === "") {
+    showErrorMessage("Please enter your last name", lastName);
+  }
+  if (lastName.validity.tooShort) {
+    transformFirstLetter(lastName);
+    showErrorMessage(
+      `Please enter your last name, must be at least ${lastName.minLength} characters`,
+      lastName
+    );
+  } else if (!regExp.test(lastName.value)) {
+    transformFirstLetter(lastName);
+    showErrorMessage(`Be sure to enter only letters`, lastName);
+  } else {
+    transformFirstLetter(lastName);
+    removeError(lastName);
   }
 });
 
 postalCode.addEventListener("input", function() {
-  if (postalCode.validity.valueMissing) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter a real postcode`;
-  } else if (postalCode.validity.tooLong) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Postcode must be not more then ${postalCode.maxlength}`;
+  if (postalCode.value === "") {
+    showErrorMessage(`Please enter a postcode`, postalCode);
+  } else if (postalCode.validity.tooShort) {
+    showErrorMessage(
+      `Postcode must be at least ${postalCode.minLength} digits long`,
+      postalCode
+    );
   } else {
-    postalCode.setCustomValidity("");
-
-    errorMessage.className = "error-message";
+    removeError(postalCode);
   }
 });
 
 phoneNumber.addEventListener("input", function() {
-  if (phoneNumber.validity.valueMissing) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter a real phone number`;
-  } else if (phoneNumber.validity.tooShort) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter a valid phone number`;
-  } else if (phoneNumber.validity.typeMismatch) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `This is not the phone number`;
-  } else {
-    phoneNumber.setCustomValidity("");
+  let phoneLength = phoneNumber.value
+    .split("+")
+    .join("")
+    .split("(")
+    .join("")
+    .split(")")
+    .join("")
+    .split(" ")
+    .join("");
 
-    errorMessage.className = "error-message";
+  if (phoneNumber.value === "") {
+    showErrorMessage(`Please enter a phone number`, phoneNumber);
+  } else if (phoneLength.length < phoneNumber.minLength) {
+    showErrorMessage(`Please enter a valid phone number`, phoneNumber);
+  } else {
+    removeError(phoneNumber);
   }
 });
 
 cardNumber.addEventListener("input", function() {
-  if (cardNumber.validity.tooShort) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter more numbers`;
-  } else if (cardNumber.validity.valueMissing) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter your card number`;
+  let cardLength = cardNumber.value.split(" ").join("");
+  if (cardNumber.value === "") {
+    showErrorMessage("Please enter your card number", cardNumber);
+  } else if (cardLength.length < cardNumber.minLength) {
+    showErrorMessage(`Please enter more digits`, cardNumber);
   } else {
-    cardNumber.setCustomValidity("");
-
-    errorMessage.className = "error-message";
+    removeError(cardNumber);
   }
 });
 
 cardFullName.addEventListener("input", () => {
-  if (cardFullName.validity.valueMissing) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter the full name `;
+  let nameLength = cardFullName.value.split(" ");
+  if (cardFullName.value === "") {
+    showErrorMessage(`Please enter the full name`, cardFullName);
+  } else if (nameLength.length < 2) {
+    cardFullName.value = cardFullName.value.toLocaleUpperCase();
+    showErrorMessage(`Please enter the full name`, cardFullName);
   } else {
-    cardFullName.setCustomValidity("");
-
-    errorMessage.className = "error-message";
+    cardFullName.value = cardFullName.value.toLocaleUpperCase();
+    removeError(cardFullName);
   }
 });
 
-cardExpiration.addEventListener("input", () => {
-  if (cardExpiration.validity.tooShort) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter a valid expiration date`;
-  } else if (cardExpiration.validity.valueMissing) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter the expiration date`;
-  } else {
-    cardExpiration.setCustomValidity("");
+let date = new Date();
+let dateYear = date.getFullYear();
+let dateMonth = date.getMonth() + 1;
 
-    errorMessage.className = "error-message";
+cardExpiration.addEventListener("input", () => {
+  let separateDates = cardExpiration.value
+    .split("/")
+    .join("")
+    .split("  ");
+  let expLength = separateDates.join("");
+  let month = separateDates[0];
+  let year = separateDates[1];
+
+  if (cardExpiration.value === "") {
+    showErrorMessage(`Please enter the expiration date`, cardExpiration);
+  } else if (expLength.length < cardExpiration.minLength) {
+    showErrorMessage(`Please enter a valid expiration date`, cardExpiration);
+  } else if (
+    month > 12 ||
+    month < 1 ||
+    month < dateMonth ||
+    year > 2100 ||
+    year < dateYear
+  ) {
+    showErrorMessage(`Please enter a valid expiration date`, cardExpiration);
+  } else {
+    removeError(cardExpiration);
   }
 });
 
 cardCvv.addEventListener("input", () => {
-  if (cardCvv.validity.tooShort) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter ${cardCvv.minLength} digits`;
-  } else if (cardCvv.validity.valueMissing) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter a CVV number`;
+  if (cardCvv.value === "") {
+    showErrorMessage("Please enter a CVV number", cardCvv);
+  } else if (cardCvv.value.length < cardCvv.minLength) {
+    showErrorMessage("Please enter a CVV number", cardCvv);
   } else {
-    cardCvv.setCustomValidity("");
-
-    errorMessage.className = "error-message";
+    removeError(cardCvv);
   }
 });
 
 tip.addEventListener("input", () => {
-  if (tip.validity.valueMissing) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please give us some money`;
-  } else if (tip.validity.tooShort) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Why so cheap? Only ${tip.value}?`;
+  let tipLength = tip.value.split(",").join("");
+  if (tip.value === "") {
+    showErrorMessage(`Please give us some money`, tip);
+  } else if (tipLength.length < tip.minLength) {
+    showErrorMessage(`Why so cheap? Only $0,${tip.value}?`, tip);
   } else {
-    tip.setCustomValidity("");
-
-    errorMessage.className = "error-message";
+    removeError(tip);
   }
 });
 
 url.addEventListener("input", () => {
-  if (url.validity.valueMissing) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `Please enter an URL`;
-  } else if (url.validity.typeMismatch) {
-    errorMessage.className = "error-message active";
-    errorMessage.textContent = `This is not an URL`;
+  if (url.validity.typeMismatch) {
+    showErrorMessage(`This is not an URL`, url);
   } else {
-    url.setCustomValidity("");
+    removeError(url);
+  }
+});
 
-    errorMessage.className = "error-message";
+filePhoto.addEventListener("change", () => {
+  let allowed = new Array("jpg", "png", "gif", "jpeg");
+  let fileExt = filePhoto.value
+    .split(".")
+    .pop()
+    .toLowerCase();
+  console.log(fileExt);
+
+  for (let i = 0; i <= allowed.length; i++) {
+    if (allowed[i] !== fileExt) {
+      showErrorMessage(`file must be a photo`, filePhoto);
+    } else if (allowed[i] == fileExt) {
+      removeError(filePhoto);
+      return;
+    }
+  }
+
+  if (filePhoto.value === "") {
+    removeError(filePhoto);
   }
 });
