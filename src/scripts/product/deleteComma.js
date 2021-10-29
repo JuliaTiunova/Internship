3d65cadb26edcee850ed64ebc07219ba7b61839b
Yuliia Tiunova <yuliia.tiunova@valtech.com>
2021-10-29 09:50:04 +0300

    add function for repeted code in deleteComma

--- src/scripts/product/deleteComma.js
+++ src/scripts/product/deleteComma.js
@@ -2,22 +2,23 @@ export function deleteComma(element) {
   if (element.length > 1) {
     element.forEach((item) => {
       let string = item.textContent;
-      string = string.split("");
-      string.splice(-2);
-      string = string.join("");
+      string = cutString(string);
       item.innerHTML = string;
     });
   } else if (element.length == 1) {
     let string = element[0].innerHTML;
-    string = string.split("");
-    string.splice(-2);
-    string = string.join("");
+    string = cutString(string);
     element[0].innerHTML = string;
   } else {
     let string = element.innerHTML;
-    string = string.split("");
-    string.splice(-2);
-    string = string.join("");
+    string = cutString(string);
     element.innerHTML = string;
   }
+}
+
+function cutString(string) {
+  string = string.split("");
+  string.splice(-2);
+  string = string.join("");
+  return string;
 }
